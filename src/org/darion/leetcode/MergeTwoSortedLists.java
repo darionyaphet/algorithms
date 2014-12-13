3645087577d74b8b0bb3424be250ce1e3d9474de
darionyaphet <darion.yaphet@gmail.com>
2014-12-13 15:57:11 +0800

    publish accepted ValidPalindrome

---
+++ src/org/darion/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,28 @@
+package org.darion.leetcode;
+
+public class MergeTwoSortedLists {
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+
+	public MergeTwoSortedLists() {
+
+	}
+
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode node = null;
+//		while(l1)
+		return null;
+	}
+
+	public static void main(String[] args) {
+		MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
+
+	}
+}
