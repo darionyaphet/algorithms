efed14cfe912f25a06ebcf42eb05d2ab59655eeb
darionyaphet <darion.yaphet@gmail.com>
2015-01-08 17:15:09 +0800

    publish CompareVersionNumbers

---
+++ src/org/darion/leetcode/MajorityElement.java
@@ -0,0 +1,13 @@
+package org.darion.leetcode;
+
+public class MajorityElement {
+	public int majorityElement(int[] num) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+		MajorityElement element = new MajorityElement();
+		System.out.println(element.majorityElement(array));
+	}
+}
