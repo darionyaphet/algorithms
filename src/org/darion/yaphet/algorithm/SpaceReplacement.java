82b3cca8873d82e352bf39135c85ad4f976096ad
darionyaphet <darion.yaphet@gmail.com>
2015-11-28 22:57:30 +0800

    append SpaceReplacement

--- src/org/darion/yaphet/algorithm/SpaceReplacement.java
+++ src/org/darion/yaphet/algorithm/SpaceReplacement.java
@@ -2,7 +2,29 @@ package org.darion.yaphet.algorithm;
 
 public class SpaceReplacement {
 
-	public static void main(String[] args) {
+	private static final Character SPACE = ' ';
 
+	public static void main(String[] args) {
+		final String source = "Hello Darion Yaphet";
+		int spaceNumber = 0;
+		for (Character element : source.toCharArray()) {
+			if (SPACE.equals(element)) {
+				spaceNumber += 1;
+			}
+		}
+
+		int index = 0;
+		char[] array = new char[source.length() + 2 * spaceNumber];
+		for (Character element : source.toCharArray()) {
+			if (!SPACE.equals(element)) {
+				array[index++] = element;
+			} else {
+				array[index++] = '%';
+				array[index++] = '2';
+				array[index++] = '0';
+			}
+		}
+
+		System.out.println(new String(array));
 	}
 }
