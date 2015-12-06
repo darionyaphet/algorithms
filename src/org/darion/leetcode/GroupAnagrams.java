7277814f4d14dd66e9ddbab77c088e5435cc895c
darionyaphet <darion.yaphet@gmail.com>
2015-12-06 11:13:51 +0800

    daily publish

---
+++ src/org/darion/leetcode/GroupAnagrams.java
@@ -0,0 +1,54 @@
+package org.darion.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class GroupAnagrams {
+	public GroupAnagrams() {
+	}
+
+	public List<List<String>> groupAnagrams(String[] strs) {
+		if (strs == null || strs.length == 0) {
+			return null;
+		}
+
+		Map<String, List<String>> map = new TreeMap<String, List<String>>();
+		for (String element : strs) {
+			char[] source = element.toCharArray();
+			Arrays.sort(source);
+			String sourceString = new String(source);
+
+			if (!map.containsKey(sourceString)) {
+				List<String> value = new LinkedList<String>();
+				value.add(element);
+				map.put(sourceString, value);
+			} else {
+				map.get(sourceString).add(element);
+			}
+		}
+
+		List<List<String>> result = new LinkedList<List<String>>();
+		for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+			List<String> inners = entry.getValue();
+			String[] innerArray = new String[inners.size()];
+			for (int index = 0; index < inners.size(); index++) {
+				innerArray[index] = inners.get(index);
+			}
+			
+			Arrays.sort(innerArray);
+			result.add(Arrays.asList(innerArray));
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		// String[] array = { "eat", "tea", "tan", "ate", "nat", "bat" };
+		String[] array = { "tea", "and", "ate", "eat", "den" };
+		GroupAnagrams instance = new GroupAnagrams();
+		System.out.println(instance.groupAnagrams(array));
+	}
+}
---
+++ src/org/darion/leetcode/ValidAnagram.java
@@ -0,0 +1,31 @@
+package org.darion.leetcode;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+	public ValidAnagram() {
+
+	}
+
+	public boolean isAnagram(String s, String t) {
+		char[] source = s.toCharArray();
+		char[] target = t.toCharArray();
+		Arrays.sort(source);
+		Arrays.sort(target);
+		String sourceString = new String(source);
+		String targetString = new String(target);
+		if (sourceString.equals(targetString)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		ValidAnagram instance = new ValidAnagram();
+		System.out.println(instance.isAnagram("anagram", "nagaram"));
+		System.out.println(instance.isAnagram("rat", "cat"));
+	}
+
+}
